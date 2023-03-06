430961eaf06c4edda1799fcfd830308862a1c6e5
JoseG-Inoa <123283416+JoseG-Inoa@users.noreply.github.com>
2023-03-05 23:55:39 -0400

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 50;
+    private Animator _ac;
+
+    private void Awake()
+    {
+        _ac = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Damage(int dmg) {
+        if(health > 0) { 
+            health -= dmg;
+
+            if (health <= 0) {
+               // _ac.Play("Death");
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
